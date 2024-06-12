8318d85ea9572a6fc08c3647f89b763038111138
SArcD <141703135+SArcD@users.noreply.github.com>
2024-06-12 16:45:49 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -57,3 +57,53 @@ if option == "Cargar Archivo":
 elif option == "Otra Funcionalidad":
     st.write("Aquí puedes agregar otra funcionalidad.")
     # Agrega el código correspondiente para la otra funcionalidad
+
+    st.subheader("Equipo de Trabajo")
+
+    # Información del equipo
+    equipo = [{
+            "nombre": "Dr. Santiago Arceo Díaz",
+            "foto": "ArceoS.jpg",
+            "reseña": "Licenciado en Física, Maestro en Física y Doctor en Ciencias (Astrofísica). Posdoctorante de la Universidad de Colima y profesor del Tecnológico Nacional de México Campus Colima. Cuenta con el perfil deseable, pertenece al núcleo académico y es colaborador del cuerpo académico Tecnologías Emergentes y Desarrollo Web de la Maestría Sistemas Computacionales. Ha dirigido tesis de la Maestría en Sistemas Computacionales y en la Maestría en Arquitectura Sostenible y Gestión Urbana.",
+            "CV": "https://scholar.google.com.mx/citations?user=3xPPTLoAAAAJ&hl=es", "contacto": "pv_sarceo@ucol.mx"},
+        {
+            "nombre": "José Ramón González",
+            "foto": "JR.jpeg",
+            "reseña": "Estudiante de la facultad de medicina en la Universidad de Colima, cursando el servicio social en investigación en el Centro Universitario de Investigaciones Biomédicas, bajo el proyecto Aplicación de un software basado en modelos predictivos como herramienta de apoyo en el diagnóstico de sarcopenia en personas adultas mayores a partir de parámetros antropométricos.", "CV": "https://scholar.google.com.mx/citations?user=3xPPTLoAAAAJ&hl=es", "contacto": "jgonzalez90@ucol.mx"},
+        {
+            "nombre": "Dra. Xochitl Angélica Rosío Trujillo Trujillo",
+            "foto": "DraXochilt.jpg",
+            "reseña": "Bióloga, Maestra y Doctora en Ciencias Fisiológicas con especialidad en Fisiología. Es Profesora-Investigadora de Tiempo Completo de la Universidad de Colima. Cuenta con perfil deseable y es miembro del Sistema Nacional de Investigadores en el nivel 3. Su línea de investigación es en Biomedicina en la que cuenta con una producción científica de más de noventa artículos en revistas internacionales, varios capítulos de libro y dos libros. Imparte docencia y ha formado a más de treinta estudiantes de licenciatura y de posgrado en programas académicos adscritos al Sistema Nacional de Posgrado del CONAHCYT.",
+        "CV": "https://portal.ucol.mx/cuib/XochitlTrujillo.htm", "contacto": "rosio@ucol.mx"},
+            {
+            "nombre": "Dr. Miguel Huerta Viera",
+            "foto": "DrHuerta.jpg",
+            "reseña": "Doctor en Ciencias con especialidad en Fisiología y Biofísica. Es Profesor-Investigador Titular “C” del Centro Universitario de Investigaciones Biomédicas de la Universidad de Colima. Es miembro del Sistema Nacional de Investigadores en el nivel 3 emérito. Su campo de investigación es la Biomedicina, con énfasis en la fisiología y biofísica del sistema neuromuscular y la fisiopatología de la diabetes mellitus. Ha publicado más de cien artículos revistas indizadas al Journal of Citation Reports y ha graduado a más de 40 Maestros y Doctores en Ciencias en programas SNP-CONAHCyT.",
+            "CV": "https://portal.ucol.mx/cuib/dr-miguel-huerta.htm", "contacto": "huertam@ucol.mx"},
+       {
+            "nombre": "Dr. Jaime Alberto Bricio Barrios",
+            "foto":  "BricioJ.jpg",
+            "reseña": "Licenciado en Nutrición, Maestro en Ciencias Médicas, Maestro en Seguridad Alimentaria y Doctor en Ciencias Médicas. Profesor e Investigador de Tiempo Completo de la Facultad de Medicina en la Universidad de Colima. miembro del Sistema Nacional de Investigadores en el nivel 1. Miembro fundador de la asociación civil DAYIN (Desarrollo de Ayuda con Investigación)",
+    "CV": "https://scholar.google.com.mx/citations?hl=es&user=ugl-bksAAAAJ", "contacto": "jbricio@ucol.mx"},      
+            {
+            "nombre": "Mtra. Elena Elsa Bricio Barrios",
+            "foto": "BricioE.jpg",
+            "reseña": "Química Metalúrgica, Maestra en Ciencias en Ingeniería Química y doctorante en Ingeniería Química. Actualmente es profesora del Tecnológico Nacional de México Campus Colima. Cuenta con el perfil deseable, es miembro del cuerpo académico Tecnologías Emergentes y Desarrollo Web y ha codirigido tesis de la Maestría en Sistemas Computacionales.",
+    "CV": "https://scholar.google.com.mx/citations?hl=es&user=TGZGewEAAAAJ", "contacto": "elena.bricio@colima.tecnm.mx"}
+    ]
+
+    #       Establecer la altura deseada para las imágenes
+    altura_imagen = 150  # Cambia este valor según tus preferencias
+
+    # Mostrar información de cada miembro del equipo
+    for miembro in equipo:
+        st.subheader(miembro["nombre"])
+        img = st.image(miembro["foto"], caption=f"Foto de {miembro['nombre']}", use_column_width=False, width=altura_imagen)
+        st.write(f"Correo electrónico: {miembro['contacto']}")
+        st.write(f"Reseña profesional: {miembro['reseña']}")
+        st.write(f"CV: {miembro['CV']}")
+
+    # Información de contacto
+    st.subheader("Información de Contacto")
+    st.write("Si deseas ponerte en contacto con nuestro equipo, puedes enviar un correo a santiagoarceodiaz@ucol.mx")
+
