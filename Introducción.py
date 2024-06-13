36ea9f4197cf9dbe461725e11046aeebefdf96a4
SArcD <141703135+SArcD@users.noreply.github.com>
2024-06-12 21:22:55 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -7,7 +7,9 @@ import io  # Asegúrate de importar el módulo io
 # Enlaces a los archivos CSV en Google Drive
 file_urls = {
     "Archivo 1": "https://drive.google.com/uc?id=1OXrglgbqvwA1Oa2aMB5iLh9bMLJNo-uu",
-    "Archivo 2": "https://drive.google.com/uc?id=1pn8-1nCeVb8piMgad-7foAI9z1nmfqsO"
+    "Archivo 2": "https://drive.google.com/uc?id=1pn8-1nCeVb8piMgad-7foAI9z1nmfqsO",
+    "Archivo 3": "https://drive.google.com/file/d/13YXjfs3Cwm9hA7OtulOcRbYpSkAVQGiz/view?usp=drive_link",
+    "Archivo 4": "https://drive.google.com/file/d/1sd2AixzBW2e48GjXJS8NSqwno92_FBxX/view?usp=drive_link"
 }
 
 # Función para descargar y cargar un archivo CSV desde Google Drive
