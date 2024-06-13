d8a6ccbad31410b275c5f1eb8f5c437833741786
SArcD <141703135+SArcD@users.noreply.github.com>
2024-06-12 21:36:29 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -9,7 +9,7 @@ file_urls = {
     "Archivo 1": "https://drive.google.com/uc?id=1OXrglgbqvwA1Oa2aMB5iLh9bMLJNo-uu",
     "Archivo 2": "https://drive.google.com/uc?id=1pn8-1nCeVb8piMgad-7foAI9z1nmfqsO",
     "Archivo 3": "https://drive.google.com/file/d/13YXjfs3Cwm9hA7OtulOcRbYpSkAVQGiz/view?usp=drive_link",
-    "Archivo 4": "https://drive.google.com/file/d/1sd2AixzBW2e48GjXJS8NSqwno92_FBxX/view?usp=drive_link"
+    "Archivo 4": "https://docs.google.com/spreadsheets/d/1mnElCAyB8NtwkV346QSizN2oiObmKsqVINpz70qyG_s/edit?usp=drive_link"
 }
 
 # Función para descargar y cargar un archivo CSV desde Google Drive
