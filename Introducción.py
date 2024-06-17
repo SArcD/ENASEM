f6d070811ef407faf9f50eebd79338704c612d23
Ramonglez1 <141690356+Ramonglez1@users.noreply.github.com>
2024-06-17 13:48:39 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -60,13 +60,10 @@ if option == "Introducción":
         
     with tab1:
             st.header("Diabetes Mellitus 2")
-            st.write (""
-
-La diabetes es una enfermedad crónica que se produce cuando el páncreas no produce suficiente insulina o cuando el cuerpo no puede utilizar de forma eficaz la insulina que produce. La insulina es una hormona que regula el azúcar en sangre. La hiperglucemia, o aumento del nivel de azúcar en sangre, es un efecto común de la diabetes no controlada y, con el tiempo, conduce a daños graves en muchos de los sistemas del cuerpo, especialmente los nervios y los vasos sanguíneos.
-
-En 2014, en la región de las Américas, el 8,3% de los adultos mayores de 18 años tenía diabetes (8,5% a nivel mundial). En 2019, la diabetes fue la causa directa de 284,049 muertes y el 44% de todas las muertes por diabetes ocurrieron antes de los 70 años (1,5 millones de muertes y el 48% antes de los 70 años, a nivel mundial).
+            st.write (""La diabetes es una enfermedad crónica que se produce cuando el páncreas no produce suficiente insulina o cuando el cuerpo no puede utilizar de forma eficaz la insulina que produce. La insulina es una hormona que regula el azúcar en sangre. La hiperglucemia, o aumento del nivel de azúcar en sangre, es un efecto común de la diabetes no controlada y, con el tiempo, conduce a daños graves en muchos de los sistemas del cuerpo, especialmente los nervios y los vasos sanguíneos. 
+            En 2014, en la región de las Américas, el 8,3% de los adultos mayores de 18 años tenía diabetes (8,5% a nivel mundial). En 2019, la diabetes fue la causa directa de 284,049 muertes y el 44% de todas las muertes por diabetes ocurrieron antes de los 70 años (1,5 millones de muertes y el 48% antes de los 70 años, a nivel mundial)."")
                    
-                   "")
+                
                     
     with tab2:
             st.header("Hipertensión arterial")
