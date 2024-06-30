a3c033c345651713d43b8caf6588b643704b1dcc
SArcD <141703135+SArcD@users.noreply.github.com>
2024-06-30 15:35:53 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -505,7 +505,7 @@ elif option == "Relaciones de Indiscernibilidad 2018":
     #df.columns = df.columns.str.replace('_18', '', regex=False).str.replace('_21', '', regex=False)
 
     st.dataframe(df)
-    st.write(f'**Descripción de la base de datos:** La base seleccionada contiene **{df.shape[0]}** filas y **{df.shape[1]}** columnas.')
+    st.write(f'*La base seleccionada contiene **{df.shape[0]}** filas y **{df.shape[1]}** columnas.*')
     
     st.write('Cada **fila** corresponde a las respuestas de una o un participante de la ENASEM y cada **columna** corresponde a un pregunta en particular de las **secciones de la "a" a la "i"** (si quiere revisar el significado de las claves de las preguntas revise la sección de "Buscador de variables"). Los registros vacíos (aquellos que muestren un **None**), los que contengan repuestas **"8" o "9"** (**"No sabe"** y **"No quiere contestar"** y los que tengan (**999**) **se eliminarán en la depuración**).')
 
