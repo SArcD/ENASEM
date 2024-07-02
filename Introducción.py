8fbe9a4c1e713795d4ab918d761db9d786226aeb
SArcD <141703135+SArcD@users.noreply.github.com>
2024-07-01 21:45:20 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -895,7 +895,7 @@ elif option == "Relaciones de Indiscernibilidad 2018":
         #datos_limpios.shape
 
 ##########################################
-    st.subheader("Clasificación de participantes.")
+    st.subheader("Sección 2: Clasificación de participantes")
     st.markdown(
         """
         En esta sección se utiliza la **Teoría de conjuntos rugosos** para agrupar a las y los participantes de la encuesta cuyas respuestas fueron idénticas. Esto se logra mediante el cálculo de las **relaciones de indiscerbibilidad** El gáfico de representa a las particiones de pacientes con respuestas idénticas en las preguntas (**'C37_18', 'H11_18', 'H15A_18', 'H5_18', 'H6_18'**).
