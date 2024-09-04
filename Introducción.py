8878c1454dd9ccdb9c948872df5c0777eea6e7f8
SArcD <141703135+SArcD@users.noreply.github.com>
2024-09-03 22:51:47 -0600

    Update Introducción.py

--- Introducción.py
+++ Introducción.py
@@ -443,30 +443,26 @@ elif option == "Relaciones de Indiscernibilidad 2018":
     )
     
     st.write("")
-    st.markdown(""" <div style="text-align: justify;"> 
+
+
+    st.markdown("""
+    <div style="text-align: justify;"> 
     El proceso se realiza en las siguientes fases:
-    
-    1. **Depuración de datos**: se eliminan datos de pacientes que no cumplan con los [criterios de inclusión](#criterios_de_inclusión) o presenten registros incompletos. Además, se definen 5 cuestionamientos de la ENASEM que guardan similitud con los que conforman el test *SARC-F* y se crea una submuestra de participantes que hayan contestado a estos cuestionamientos.
-
-    2. **Clasificación de participantes**: Usando la [**teoría de conjuntos rugosos**](https://shre.ink/DjYi), se divide la base de datos en una colección de subconjuntos de pacientes que hayan contestado idénticamente a las preguntas clave (a estos subconjuntos se les llama relaciones de indiscernibilidad).
-
-    3. **Obtención de reglas de decisión**: Se entrena un modelo de árbol de decisión para determinar un conjunto de reglas que permitan clasificar a los pacientes de la base de datos (aún aquellos que inicialmente no tenían respuestas completas en todas las preguntas de interés).
-</div> """,  unsafe_allow_html=True)
-
-    st.markdown(
-    """
-    <div style="text-align: justify;">
-        El proceso se realiza en las siguientes fases:
-        
-        1. <strong>Depuración de datos</strong>: se eliminan datos de pacientes que no cumplan con los <a href="#criterios_de_inclusión">criterios de inclusión</a> o presenten registros incompletos. Además, se definen 5 cuestionamientos de la ENASEM que guardan similitud con los que conforman el test <em>SARC-F</em> y se crea una submuestra de participantes que hayan contestado a estos cuestionamientos.
-
-        2. <strong>Clasificación de participantes</strong>: Usando la <a href="https://shre.ink/DjYi"><strong>teoría de conjuntos rugosos</strong></a>, se divide la base de datos en una colección de subconjuntos de pacientes que hayan contestado idénticamente a las preguntas clave (a estos subconjuntos se les llama relaciones de indiscernibilidad).
-
-        3. <strong>Obtención de reglas de decisión</strong>: Se entrena un modelo de árbol de decisión para determinar un conjunto de reglas que permitan clasificar a los pacientes de la base de datos (aún aquellos que inicialmente no tenían respuestas completas en todas las preguntas de interés).
     </div>
-    """,
-    unsafe_allow_html=True
-    )
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+    1. **Depuración de datos**: se eliminan datos de pacientes que no cumplan con los [criterios de inclusión](#criterios_de_inclusión) o presenten registros      incompletos. Además, se definen 5 cuestionamientos de la ENASEM que guardan similitud con los que conforman el test *SARC-F* y se crea una submuestra de         participantes que hayan contestado a estos cuestionamientos.
+    """)
+
+    st.markdown("""
+    2. **Clasificación de participantes**: Usando la [**teoría de conjuntos rugosos**](https://shre.ink/DjYi), se divide la base de datos en una colección de     subconjuntos de pacientes que hayan contestado idénticamente a las preguntas clave (a estos subconjuntos se les llama relaciones de indiscernibilidad).
+    """)
+
+    st.markdown("""
+    3. **Obtención de reglas de decisión**: Se entrena un modelo de árbol de decisión para determinar un conjunto de reglas que permitan clasificar a los     pacientes de la base de datos (aún aquellos que inicialmente no tenían respuestas completas en todas las preguntas de interés).
+    """)
+
 
 
     
