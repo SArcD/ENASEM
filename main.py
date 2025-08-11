2ed1032030d8796eafc75d1885d2b44a5af14256
SArcD <141703135+SArcD@users.noreply.github.com>
2025-08-11 17:21:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -735,8 +735,12 @@ for c in comorb_cols_presentes:
     df_work[c] = pd.to_numeric(df_work[c], errors="coerce").fillna(0)
 
 # 🔹 Convertir respuestas 6 o 7 en las columnas H a 1
-cols_H = [col for col in df_work.columns if col.startswith("H")]
-df_work[cols_H] = df_work[cols_H].replace({6: 1, 7: 1})
+#cols_H = [col for col in df_work.columns if col.startswith("H")]
+#df_work[cols_H] = df_work[cols_H].replace({6: 1, 7: 1})
+
+# Convertir respuestas 6 o 7 en columnas H a 1 en la base final
+cols_H = [col for col in st.session_state["df_comorb"].columns if col.startswith("H")]
+st.session_state["df_comorb"][cols_H] = st.session_state["df_comorb"][cols_H].replace({6: 1, 7: 1})
 
 
 # HAsta aqui el filtrado
